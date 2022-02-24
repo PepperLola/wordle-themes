478086f4cb17510e915112951710f6840ffca4bc
PepperLola <julian.orion.wright@gmail.com>
2022-02-24 11:21:01 -0800

    iterate over every tile on keyboard update to properly set custom colors, although they only change after all the row is revealed

--- main.js
+++ main.js
@@ -33,22 +33,54 @@ let defaultColors = {
 
 // set custom colors
 let gameRows = gameApp.shadowRoot.querySelectorAll('game-row');
-let keyboardButtons = gameApp.shadowRoot
-	.querySelector('game-keyboard')
-	.shadowRoot.querySelectorAll('button');
-browser.storage.sync
-	.get(['correctColor', 'presentColor', 'absentColor'])
-	.then((stored) => {
-		let darkMode = localStorage.getItem('nyt-wordle-darkmode') == 'true';
-		let highContrast = localStorage.getItem('nyt-wordle-cbmode') == 'true';
-		let contrastMode = highContrast ? 'highContrast' : 'default';
-		let mode = darkMode ? 'dark' : 'light';
-		gameRows.forEach((row) => {
-			row.shadowRoot.querySelectorAll('game-tile').forEach((tile) => {
-				let element = tile.shadowRoot.querySelector('div.tile');
+let gameKeyboard = gameApp.shadowRoot.querySelector('game-keyboard');
+let keyboardButtons = gameKeyboard.shadowRoot.querySelectorAll('button');
 
+// day of first wordle problem?
+let startDay = new Date(2021, 5, 19, 0, 0, 0, 0);
+
+// get the number of days between the first wordle problem and today
+const getOffset = () => {
+	let currentDate = new Date();
+	let difference = currentDate.setHours(0, 0, 0, 0) - startDay;
+	return Math.round(difference / 864e5);
+};
+
+let colorObserver = new MutationObserver((mutationList, observer) => {
+	let mutated = mutationList
+		.filter((btn) => btn.attributeName == 'data-state')
+		.map((btn) => btn.target);
+	// second expression is dumb but we have to find elements one at a time because of shadowRoot
+	// finds each row, their shadow roots, then each tile in the row, then their shadow roots,
+	// then the actual tile element with the letter
+	// then reduces because the previous expression returns an array of arrays
+	// then removes the ones that don't have a data state because we don't change their colors
+	// which might be faster than leaving them?
+	mutated = [
+		...mutated,
+		...Array.from(gameApp.shadowRoot.querySelectorAll('game-row'))
+			.map((row) => row.shadowRoot)
+			.map((row) =>
+				Array.from(row.querySelectorAll('game-tile'))
+					.map((row) => row.shadowRoot)
+					.map((tile) => tile.querySelector('div.tile'))
+			)
+			.reduce((a, b) => [...a, ...b])
+			.filter((element) => element.getAttribute('data-state') != ''),
+	];
+	if (mutated.length < 1) return;
+	browser.storage.sync
+		.get(['correctColor', 'presentColor', 'absentColor'])
+		.then((stored) => {
+			let darkMode =
+				localStorage.getItem('nyt-wordle-darkmode') == 'true';
+			let highContrast =
+				localStorage.getItem('nyt-wordle-cbmode') == 'true';
+			let contrastMode = highContrast ? 'highContrast' : 'default';
+			let mode = darkMode ? 'dark' : 'light';
+			mutated.forEach((element) => {
 				let color = '#121213';
-				let evaluation = tile.getAttribute('evaluation');
+				let evaluation = element.getAttribute('data-state');
 				switch (evaluation) {
 					case 'correct':
 						color =
@@ -68,47 +100,16 @@ browser.storage.sync
 						break;
 				}
 
-				element.style = `background: ${color}`;
+				element.style.background = color;
 			});
 		});
+});
 
-		keyboardButtons.forEach((element) => {
-			let color = '#d3d6da';
-			let evaluation = element.getAttribute('data-state');
-			switch (evaluation) {
-				case 'correct':
-					color =
-						stored['correctColor'] ||
-						defaultColors[contrastMode][mode][evaluation];
-					break;
-				case 'present':
-					color =
-						stored['presentColor'] ||
-						defaultColors[contrastMode][mode][evaluation];
-					break;
-				case 'absent':
-					color =
-						stored['absentColor'] ||
-						defaultColors[contrastMode][mode][evaluation];
-					break;
-				default:
-					color = defaultColors[contrastMode][mode]['keyboard'];
-					break;
-			}
-
-			element.style = `background: ${color}`;
-		});
-	});
-
-// day of first wordle problem?
-let startDay = new Date(2021, 5, 19, 0, 0, 0, 0);
-
-// get the number of days between the first wordle problem and today
-const getOffset = () => {
-	let currentDate = new Date();
-	let difference = currentDate.setHours(0, 0, 0, 0) - startDay;
-	return Math.round(difference / 864e5);
-};
+colorObserver.observe(gameKeyboard.shadowRoot, {
+	attributes: true,
+	childList: true,
+	subtree: true,
+});
 
 // share button doesn't exist until stats container is created, so we listen for that
 const observer = new MutationObserver(() => {
